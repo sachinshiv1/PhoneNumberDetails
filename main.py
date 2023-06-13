5048faf9ad16bace89a23cdadca4e3241419952b
Sachin <itssachinshivanand@gmail.com>
2023-06-13 14:50:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import phonenumbers
+    # phonenumbers module have many functions to get details
 from phonenumbers import timezone, geocoder, carrier
 
 number = input("Enter your Number with Country code\n")
+    # Take Number from the user in String form
+
 phone = phonenumbers.parse(number)
 time = timezone.time_zones_for_number(phone)
 car = carrier.name_for_number(phone, "en")
